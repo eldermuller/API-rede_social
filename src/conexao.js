6d6b592a0d60fcbe9dbb9a042a275950c31be4f8
Élder Müller <elder.muller@gmail.com>
2022-05-06 16:34:02 -0300

    dotenv implementado no batabase

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'postgres',
-        database: 'mini_insta'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     }
 });
 
